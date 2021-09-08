bd7762ef08499968ade49f3ed6775672c279f525
Kip Cole <kipcole9@gmail.com>
2021-09-08 18:29:07 +0800

    Don't convert dates to iso days

--- lib/cldr/utils/calendar_conversion.ex
+++ lib/cldr/utils/calendar_conversion.ex
@@ -26,6 +26,7 @@ defmodule Cldr.Calendar.Conversion do
   end
 
   defp adjust_era(dates) do
+    IO.inspect dates
     Enum.map(dates, fn
       {"start", date} -> {"start", to_iso_days(date)}
       {"end", date} -> {"end", to_iso_days(date)}
@@ -56,7 +57,7 @@ defmodule Cldr.Calendar.Conversion do
   end
 
   def to_iso_days(%{year: year, month: month, day: day}) do
-    gregorian_date_to_iso_days(year, month, day)
+    [year, month, day]
   end
 
   def to_iso_days(date) when is_binary(date) do
@@ -69,46 +70,8 @@ defmodule Cldr.Calendar.Conversion do
           {String.to_integer("-#{year}"), String.to_integer(month), String.to_integer(day)}
       end
 
-    gregorian_date_to_iso_days(year, month, day)
+    [year, month, day]
   end
 
-  @doc """
-  Converts a `year`, `month` and `day` into a number of days
-  for the gregorian calendar
 
-  This should be done in the Calendar.ISO module but today that
-  module doesnt handle negative years which are needed here.
-  """
-  def gregorian_date_to_iso_days(year, month, day) do
-    correction =
-      cond do
-        month <= 2 -> 0
-        leap_year?(year) -> -1
-        true -> -2
-      end
-
-    (gregorian_epoch_days() - 1 + 365 * (year - 1) + Float.floor((year - 1) / 4) -
-       Float.floor((year - 1) / 100) + Float.floor((year - 1) / 400) +
-       Float.floor((367 * month - 362) / 12) + correction + day)
-    |> trunc
-  end
-
-  @doc """
-  Returns true if the given year is a leap year.
-  """
-  def leap_year?(year) when is_integer(year) do
-    mod(year, 4) === 0 and (mod(year, 100) > 0 or mod(year, 400) === 0)
-  end
-
-  defp gregorian_epoch_days do
-    1
-  end
-
-  def mod(x, y) when is_integer(x) and is_integer(y) do
-    mod(x * 1.0, y) |> round
-  end
-
-  def mod(x, y) do
-    x - y * Float.floor(x / y)
-  end
 end
